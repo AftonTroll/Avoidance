24095f182b33ef909e887273d8e30020c400b1f2
AftonTroll <filbry_92@hotmail.com>
2012-09-22 22:21:22 +0200

    Added a SensorEventListener in PlayerControlSystem
    PlayerControlSystem now listens to the accelerometer and stores the last
    sensor values

--- src/se/chalmers/avoidance/PlayerControlSystem.java
+++ src/se/chalmers/avoidance/PlayerControlSystem.java
@@ -2,6 +2,9 @@ package se.chalmers.avoidance;
 
 import se.chalmers.avoidance.components.Transform;
 import se.chalmers.avoidance.components.Velocity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 
 import com.artemis.Aspect;
@@ -10,10 +13,18 @@ import com.artemis.systems.EntityProcessingSystem;
 
 public class PlayerControlSystem extends EntityProcessingSystem {
 	private SensorManager sensorManager;
+	private float lastAccelerometerX = 0;
+	private float lastAccelerometerY = 0;
 	
 	public PlayerControlSystem(SensorManager sensorManager) {
 		super(Aspect.getAspectForAll(Transform.class, Velocity.class));
 		this.sensorManager = sensorManager;
+	}
+	
+	@Override
+	protected void initialize() {
+		Sensor accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		sensorManager.registerListener(new AccelerometerListener(), accelerometer,SensorManager.SENSOR_DELAY_GAME);
 	}
 
 	@Override
@@ -23,6 +34,20 @@ public class PlayerControlSystem extends EntityProcessingSystem {
 
 	@Override
 	protected void process(Entity entity) {
+		
+	}
+	
+	
+	private class AccelerometerListener implements SensorEventListener {
+
+		public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+		}
+
+		public void onSensorChanged(SensorEvent event) {
+			lastAccelerometerX = event.values[0];
+			lastAccelerometerY = event.values[1];
+		}
 		
 	}
 
