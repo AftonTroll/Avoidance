eaa60f0f993c97d1e0f21bdaab87519d603d9c7a
Florian Minges <florianminges@hotmail.com>
2012-09-22 12:03:03 +0200

    Added a size component
    Still missing javadoc and tests.

--- src/se/chalmers/avoidance/components/Size.java
+++ src/se/chalmers/avoidance/components/Size.java
@@ -1,5 +1,50 @@
 package se.chalmers.avoidance.components;
 
+/**
+ * Component containing information about the size.
+ * 
+ * @author Florian Minges
+ *
+ */
 public class Size {
+	private float width;
+	private float height;
+	
+	public Size() {
+		this(1, 1);
+	}
+	
+	public Size(float width, float height) {
+		setSize(width, height);
+	}
+	
+	public float getWidth() {
+		return this.width;
+	}
+	
+	public float getHeight() {
+		return this.height;
+	}
+	
+	public void setSize(float width, float height) {
+		setWidth(width);
+		setHeight(height);
+	}
+	
+	public void setWidth(float width) {
+		this.width = width;
+	}
+	
+	public void setHeight(float height) {
+		this.height = height;
+	}
+	
+	public void addWidth(float width) {
+		this.width += width;
+	}
+	
+	public void addHeight(float height) {
+		this.height += height;
+	}
 
 }
