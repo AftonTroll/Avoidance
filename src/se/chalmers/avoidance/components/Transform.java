f2aa16f1fb6afeb4216310c953eb78f2241326f3
Florian Minges <florianminges@hotmail.com>
2012-09-22 15:41:47 +0200

    Added javadoc and renamed some variables and methods
    Variables:
    rotation -> direction
    
    Methods:
    getRotation -> getDirection
    setRotation -> setDirection
    addRotation -> translateDirection
    addX -> translateX
    addY -> translateY

--- src/se/chalmers/avoidance/components/Transform.java
+++ src/se/chalmers/avoidance/components/Transform.java
@@ -5,21 +5,21 @@ import se.chalmers.avoidance.util.Utils;
 import com.artemis.Component;
 
 /**
- * A transform component. Contains information about position and rotation.
+ * Component containing information about position and direction.
  * 
  * @author Florian Minges
  */
 public class Transform extends Component {
 
-	/** The x-coordinate of this position. */
+	/** The x-coordinate of the position. */
 	private float x;
 	
-	/** The y-coordinate of this position. */
+	/** The y-coordinate of the position. */
 	private float y;
 	
 	
 	/** 
-	 * The rotation in radians. <p>
+	 * Represents a direction measured in radians.<p>
 	 * 
 	 * 0				=>	right <br>
 	 * pi / 2			=> 	up <br>
@@ -27,23 +27,23 @@ public class Transform extends Component {
 	 * (3 * pi) / 2		=>	down
 	 * 
 	 */
-	private float rotation;
+	private float direction;
 	
 	
 	
 	/**
 	 * Creates a Transform component with default position (0, 0)
-	 * and default rotation 0 (right). 
+	 * and default direction 0 (right). 
 	 */
 	public Transform() {
 		this(0f, 0f, 0f);
 	}
 	
 	/**
-	 * Creates a Transform component with default rotation 0 (right).
+	 * Creates a Transform component with default direction 0 (right).
 	 * 
-	 * @param x The x-coordinate.
-	 * @param y The y-coordinate.
+	 * @param x the x-coordinate 
+	 * @param y the y-coordinate 
 	 */
 	public Transform(float x, float y) {
 		this(x, y, 0);
@@ -51,56 +51,104 @@ public class Transform extends Component {
 	
 	
 	/**
-	 * Creates a Transform component with the specified position and rotation.
+	 * Creates a Transform component with the specified position and direction.
 	 * 
-	 * @param x The x-coordinate.
-	 * @param y The y-coordinate.
-	 * @param rotation The {@link #rotation rotation}.
+	 * @param x the x-coordinate 
+	 * @param y the y-coordinate 
+	 * @param direction the {@link #direction direction} 
 	 */
-	public Transform(float x, float y, float rotation) {
+	public Transform(float x, float y, float direction) {
 		setPosition(x, y);
-		setRotation(rotation);
+		setDirection(direction);
 	}
 	
+	/**
+	 * Returns the x-coordinate.
+	 * @return the x-coordinate.
+	 */
 	public float getX() {
 		return x;
 	}
 	
+	/**
+	 * Returns the y-coordinate.
+	 * @return the y-coordinate.
+	 */
 	public float getY() {
 		return y;
 	}
 	
-	public float getRotation() {
-		return rotation;
+	/**
+	 * Return the direction.
+	 * @return the direction.
+	 */
+	public float getDirection() {
+		return direction;
 	}
 	
+	/**
+	 * Sets the x and y-coordinate of this component.
+	 * 
+	 * @param x the x-coordinate
+	 * @param y the y-coordinate
+	 */
 	public void setPosition(float x, float y) {
 		setX(x);
 		setY(y);
 	}
 	
+	/**
+	 * Sets the x-coordinate of this component.
+	 * 
+	 * @param x the x-coordinate
+	 */
 	public void setX(float x) {
 		this.x = x;
 	}
 	
+	/**
+	 * Sets the y-coordinate of this component.
+	 * 
+	 * @param y the y-coordinate
+	 */
 	public void setY(float y) {
 		this.y = y;
 	}
 	
-	public void addX(float x) {
+	/**
+	 * Moves the x-coordinate of this component by the given argument.
+	 *  
+	 * @param x the distance to move along the x-axis
+	 */
+	public void translateX(float x) {
 		this.x += x;
 	}
 	
-	public void addY(float y) {
+	/**
+	 * Moves the y-coordinate of this component by the given argument.
+	 *  
+	 * @param y the distance to move along the y-axis
+	 */
+	public void translateY(float y) {
 		this.y += y;
 	}
 	
-	public void setRotation(float rotation) {
-		this.rotation = Utils.simplifyAngle(rotation);
+	/**
+	 * Sets the direction of this component.
+	 * 
+	 * @param direction the direction
+	 */
+	public void setDirection(float direction) {
+		this.direction = Utils.simplifyAngle(direction);
 	}
 	
-	public void addRotation(float rotation) {
-		this.rotation = Utils.simplifyAngle(this.rotation + rotation);
+	/**
+	 * Rotates the direction of this component by the given argument.
+	 * 
+	 * @param direction the degree to rotate in radians
+	 */
+	public void translateDirection(float direction) {
+		this.direction = Utils.simplifyAngle(this.direction + direction);
 	}
 	
 }
