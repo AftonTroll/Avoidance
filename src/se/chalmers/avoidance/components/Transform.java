30d83567a74c02ce2ade3783229ce39daf556744
Florian Minges <florianminges@hotmail.com>
2012-09-22 12:02:01 +0200

    Added a transform component
    Some javadoc and tests are still missing.
    We also might want to take a look at the rotation member variable, which currently is a double (mostly due to the Math.PI-constant which too is a double). We might want to consider changing this to be a float, so that it requires less memory.

--- src/se/chalmers/avoidance/components/Transform.java
+++ src/se/chalmers/avoidance/components/Transform.java
@@ -1,5 +1,114 @@
 package se.chalmers.avoidance.components;
 
-public class Transform {
+import com.artemis.Component;
 
+/**
+ * A transform component. Contains information about position and rotation.
+ * 
+ * @author Florian Minges
+ */
+public class Transform extends Component {
+
+	/** The x-coordinate of this position. */
+	private float x;
+	
+	/** The y-coordinate of this position. */
+	private float y;
+	
+	
+	/** 
+	 * The rotation in radians. <p>
+	 * 
+	 * 0				=>	right <br>
+	 * pi / 2			=> 	up <br>
+	 * pi				=>	left <br>
+	 * (3 * pi) / 2		=>	down
+	 * 
+	 */
+	private double rotation;
+	
+	
+	
+	/**
+	 * Creates a Transform component with default position (0, 0)
+	 * and default rotation 0 (right). 
+	 */
+	public Transform() {
+		this(0f, 0f, 0);
+	}
+	
+	/**
+	 * Creates a Transform component with default rotation 0 (right).
+	 * 
+	 * @param x The x-coordinate.
+	 * @param y The y-coordinate.
+	 */
+	public Transform(float x, float y) {
+		this(x, y, 0);
+	}
+	
+	
+	/**
+	 * Creates a Transform component with the specified position and rotation.
+	 * 
+	 * @param x The x-coordinate.
+	 * @param y The y-coordinate.
+	 * @param rotation The {@link #rotation rotation}.
+	 */
+	public Transform(float x, float y, double rotation) {
+		setPosition(x, y);
+		setRotation(rotation);
+	}
+	
+	public float getX() {
+		return x;
+	}
+	
+	public float getY() {
+		return y;
+	}
+	
+	public double getRotation() {
+		return rotation;
+	}
+	
+	public void setPosition(float x, float y) {
+		setX(x);
+		setY(y);
+	}
+	
+	public void setX(float x) {
+		this.x = x;
+	}
+	
+	public void setY(float y) {
+		this.y = y;
+	}
+	
+	public void addX(float x) {
+		this.x += x;
+	}
+	
+	public void addY(float y) {
+		this.y += y;
+	}
+	
+	public void setRotation(double rotation) {
+		this.rotation = rotation;
+		simplifyRotation();
+	}
+	
+	public void addRotation(float rotation) {
+		this.rotation += rotation;
+		simplifyRotation();
+	}
+	
+	/**
+	 *  Converts the rotation so its' value is in the interval
+	 *  0  <=  rotation  <  (2 * PI)
+	 */
+	private void simplifyRotation() {
+		this.rotation = this.rotation % (2 * Math.PI);
+	}
+	
 }
