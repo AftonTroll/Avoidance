5f1d42b5b0251819719dbf5efd527c6fc8f790fa
Jakob Svensson <jakob-_-92@hotmail.com>
2012-09-23 19:38:57 +0200

    Implemented a simple version of processEntitites
    The players angle is add by PI when the player collides with a wall

--- src/se/chalmers/avoidance/systems/CollisionSystem.java
+++ src/se/chalmers/avoidance/systems/CollisionSystem.java
@@ -1,8 +1,6 @@
 package se.chalmers.avoidance.systems;
 
 import org.andengine.entity.shape.RectangularShape;
-import org.andengine.entity.sprite.Sprite;
-import org.andengine.opengl.shader.ShaderProgram;
 import org.andengine.opengl.vbo.IVertexBufferObject;
 
 import se.chalmers.avoidance.components.Size;
@@ -10,8 +8,15 @@ import se.chalmers.avoidance.components.Transform;
 import se.chalmers.avoidance.components.Velocity;
 
 import com.artemis.Aspect;
+import com.artemis.Component;
+import com.artemis.ComponentMapper;
+import com.artemis.ComponentType;
 import com.artemis.Entity;
 import com.artemis.EntitySystem;
+import com.artemis.World;
+import com.artemis.annotations.Mapper;
+import com.artemis.managers.GroupManager;
+import com.artemis.managers.TagManager;
 import com.artemis.utils.ImmutableBag;
 /**
  * System for handling collision between entities
@@ -20,12 +25,22 @@ import com.artemis.utils.ImmutableBag;
  *
  */
 public class CollisionSystem extends EntitySystem{
-
-	public CollisionSystem() {
-		super(Aspect.getAspectForAll(Transform.class, Velocity.class));
-		// TODO Auto-generated constructor stub
+	
+	private ComponentMapper<Transform> transformMapper;
+    private ComponentMapper<Velocity> velocityMapper;
+	
+	public CollisionSystem(World world) {
+		super(Aspect.getAspectForAll(Transform.class, Size.class));
+		this.world = world;
 	}
+	
+	@Override
+	public void initialize(){
+		transformMapper = world.getMapper(Transform.class);
+		velocityMapper = world.getMapper(Velocity.class);
 
+	}
+	
 	@Override
 	protected boolean checkProcessing() {
 		// TODO Auto-generated method stub
@@ -35,8 +50,25 @@ public class CollisionSystem extends EntitySystem{
 	@Override
 	protected void processEntities(ImmutableBag<Entity> entities) {
 		
-		//Check collision between player and wall
+		ImmutableBag<Entity> walls = world.getManager(GroupManager.class).getEntities("Walls");
+		Entity player = world.getManager(TagManager.class).getEntity("Player");
+		System.out.println(walls.get(0));
+		System.out.println(player);
+		
+		for (int i=0;i<walls.size();i++){
+			if(collisionExists(player, walls.get(i))){
+				Velocity velocity = velocityMapper.get(player);
+				velocity.setAngle(velocity.getAngle()+(float)Math.PI);
+				System.out.println("here");
+				System.out.println(velocity.getAngle());
+				System.out.println(player.getComponent(Velocity.class).getAngle());
+			}
+		}
+		
 	}
+	
+	
+		
 	/**
 	 * Checks if two entities is colliding with each other 
 	 * 
@@ -71,7 +103,7 @@ public class CollisionSystem extends EntitySystem{
 	 *
 	 */
 	private class CollisionObject extends RectangularShape{
-
+		
 		public CollisionObject(float pX, float pY, float pWidth, float pHeight) {
 			super(pX, pY, pWidth, pHeight, null);
 		}
