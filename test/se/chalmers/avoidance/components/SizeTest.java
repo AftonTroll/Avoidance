80e33e4647c34e8405c172daddd913aa9b76aa2f
Florian Minges <florianminges@hotmail.com>
2012-09-23 22:42:57 +0200

    Removed unused import

--- test/se/chalmers/avoidance/components/SizeTest.java
+++ test/se/chalmers/avoidance/components/SizeTest.java
@@ -1,7 +1,6 @@
 package se.chalmers.avoidance.components;
 
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 import org.junit.Before;
 import org.junit.BeforeClass;
