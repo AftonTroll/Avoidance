79fa1ce6ca13a77086f1d425faa4c82634cf5598
Heqir <markus_xtrom@hotmail.com>
2012-09-27 16:22:33 +0200

    Added license and some javadoc
    EntityFactory and MainActivity had no javadoc

--- src/se/chalmers/avoidance/EntityFactory.java
+++ src/se/chalmers/avoidance/EntityFactory.java
@@ -1,3 +1,23 @@
+/* 
+ * Copyright (c) 2012 Markus Ekström
+ * 
+ * This file is part of Avoidance.
+ * 
+ * Avoidance is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Avoidance is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Avoidance.  If not, see <http://www.gnu.org/licenses/>. 
+ *  
+ */
+
 package se.chalmers.avoidance;
 
 import se.chalmers.avoidance.components.Size;
@@ -8,9 +28,21 @@ import se.chalmers.avoidance.components.Velocity;
 import com.artemis.Entity;
 import com.artemis.World;
 import com.artemis.managers.TagManager;
-
+/**
+ * A factory for entities. 
+ * 
+ * All entities are created through this factory.
+ * 
+ * @author Markus Ekström
+ */
 public class EntityFactory {
 	
+	/**
+	 * Creates a player entity with a PLAYER tag.
+	 * 
+	 * @param world The world.
+	 * @return An Entity (the player entity).
+	 */
 	public static Entity createPlayer(World world){
 		Entity player = world.createEntity();
 		world.getManager(TagManager.class).register("PLAYER", player);
