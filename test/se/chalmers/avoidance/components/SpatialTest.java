c5ca989757b03f7c1dfea7046b04776afd612bbc
Heqir <markus_xtrom@hotmail.com>
2012-09-27 14:09:19 +0200

    Added test for Spatial.

---
+++ test/se/chalmers/avoidance/components/SpatialTest.java
@@ -0,0 +1,45 @@
+package se.chalmers.avoidance.components;
+
+import static org.junit.Assert.*;
+
+import org.andengine.entity.sprite.Sprite;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class SpatialTest {
+	private static String name;
+	private static Sprite sprite;
+	
+	private Spatial spatial;
+	
+	@BeforeClass
+	public static void beforeClass() throws Exception {
+		name = "test";
+		sprite = new Sprite(0, 0, 0, 0, null, null, null, null);
+	}
+	
+	@Before
+	public void setUp() throws Exception {
+		spatial = new Spatial(name);
+	}
+	
+	@Test
+	public void testSpatial() {
+		assertTrue(spatial != null);
+	}
+
+	@Test
+	public void testNameSetterAndGetter() {
+		assertTrue(spatial.getName() == name);
+		
+		spatial.setName("testName");
+		assertTrue(spatial.getName() == "testName");
+	}
+	
+	@Test
+	public void testSpriteSetterGetter() {
+		spatial.setSprite(sprite);
+		assertTrue(spatial.getSprite() == sprite);
+	}
+}
