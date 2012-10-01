10f34556de5581fee9b397ceffd40eda042245a4
AftonTroll <filbry_92@hotmail.com>
2012-10-01 16:40:14 +0200

    Added a spawning system

---
+++ src/se/chalmers/avoidance/core/systems/SpawnSystem.java
@@ -0,0 +1,20 @@
+package se.chalmers.avoidance.core.systems;
+
+import com.artemis.Aspect;
+import com.artemis.Entity;
+import com.artemis.systems.EntityProcessingSystem;
+
+public class SpawnSystem extends EntityProcessingSystem{
+
+	public SpawnSystem(Aspect aspect) {
+		super(aspect);
+		
+	}
+
+	@Override
+	protected void process(Entity entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
