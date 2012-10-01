0fc47fbb988afb87995ca7dad0bda84f10c02acc
Jakob Svensson <jakob-_-92@hotmail.com>
2012-10-01 11:35:50 +0200

    Added some methods in Time.java

--- src/se/chalmers/avoidance/core/components/Time.java
+++ src/se/chalmers/avoidance/core/components/Time.java
@@ -2,6 +2,15 @@ package se.chalmers.avoidance.core.components;
 
 import com.artemis.Component;
 
-public class Time extends Component{
-
+public class Time extends Component {
+	
+	private float currentTime=0;
+	
+	public float getTime(){
+		return currentTime;
+	}
+	
+	public void updateTime(float tpf){
+		currentTime+=tpf;
+	}
 }
