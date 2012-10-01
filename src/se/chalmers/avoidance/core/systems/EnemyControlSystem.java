efddf6964a09a8d2adda48ab77baa7599b717dbc
AftonTroll <filbry_92@hotmail.com>
2012-10-01 13:03:22 +0200

    Added mappers in EnemyControlSystem
    Added transformMapper, sizeMapper and elocityMapper

--- src/se/chalmers/avoidance/core/systems/EnemyControlSystem.java
+++ src/se/chalmers/avoidance/core/systems/EnemyControlSystem.java
@@ -1,18 +1,26 @@
 package se.chalmers.avoidance.core.systems;
 
+import se.chalmers.avoidance.core.components.Size;
 import se.chalmers.avoidance.core.components.Transform;
 import se.chalmers.avoidance.core.components.Velocity;
 
 import com.artemis.Aspect;
+import com.artemis.ComponentMapper;
 import com.artemis.Entity;
 import com.artemis.EntitySystem;
+import com.artemis.managers.GroupManager;
 import com.artemis.managers.TagManager;
 import com.artemis.utils.ImmutableBag;
 
 public class EnemyControlSystem extends EntitySystem{
-
+	private ComponentMapper<Transform> transformMapper;
+	private ComponentMapper<Velocity> velocityMapper;
+	private ComponentMapper<Size> sizeMapper;
+	private TagManager tagManager;
+	private GroupManager groupManager;
+	
 	public EnemyControlSystem() {
-		super(Aspect.getAspectForAll(Transform.class, Velocity.class));
+		super(Aspect.getAspectForAll(Transform.class, Velocity.class, Size.class));
 	}
 	
 	/**
@@ -20,7 +28,11 @@ public class EnemyControlSystem extends EntitySystem{
 	 */
 	@Override
 	protected void initialize() {
-		
+		transformMapper = world.getMapper(Transform.class);
+		velocityMapper = world.getMapper(Velocity.class);
+		sizeMapper = world.getMapper(Size.class);
+		tagManager = world.getManager(TagManager.class);
+		groupManager = world.getManager(GroupManager.class);
 	}
 
 	@Override
@@ -29,8 +41,7 @@ public class EnemyControlSystem extends EntitySystem{
 	}
 
 	@Override
-	protected void processEntities(ImmutableBag<Entity> arg0) {
-		
-	}
-	
+	protected void processEntities(ImmutableBag<Entity> bag) {
+
+	}	
 }
