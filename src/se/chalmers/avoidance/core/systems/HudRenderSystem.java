1f8480dc8b5d8a919f2622fc6b06dada9e3469e9
Jakob Svensson <jakob-_-92@hotmail.com>
2012-10-03 15:38:06 +0200

    Implemented HudRenderSystem

--- src/se/chalmers/avoidance/core/systems/HudRenderSystem.java
+++ src/se/chalmers/avoidance/core/systems/HudRenderSystem.java
@@ -1,30 +1,52 @@
 package se.chalmers.avoidance.core.systems;
 
-import se.chalmers.avoidance.core.components.Spatial;
+import org.andengine.entity.scene.Scene;
+import org.andengine.entity.text.Text;
+import org.andengine.opengl.font.Font;
+import org.andengine.opengl.vbo.VertexBufferObjectManager;
+
 import se.chalmers.avoidance.core.components.Time;
-import se.chalmers.avoidance.core.components.Transform;
 
 import com.artemis.Aspect;
+import com.artemis.ComponentMapper;
 import com.artemis.Entity;
-import com.artemis.EntitySystem;
-import com.artemis.utils.ImmutableBag;
+import com.artemis.systems.EntityProcessingSystem;
 
-public class HudRenderSystem extends EntitySystem{
-
-	public HudRenderSystem() {
-		super(Aspect.getAspectForAll(Time.class, Transform.class, Spatial.class));
+public class HudRenderSystem extends EntityProcessingSystem{
+	
+	private Font font;
+	private Text score;
+	private Scene scene;
+	private VertexBufferObjectManager vbom;
+	private ComponentMapper<Time> timeMapper;
+	
+	public HudRenderSystem(Scene scene, VertexBufferObjectManager vbom, Font font) {
+		super(Aspect.getAspectForAll(Time.class));
+		this.font = font;	
+		this.scene = scene;
+		this.vbom = vbom;
 	}
-
+	
+	/**
+	 * This method is called when the system is initialized
+	 */
+	@Override
+	protected void initialize(){
+		timeMapper = world.getMapper(Time.class);
+		this.score = new Text(100, 160, this.font, "Score :", "Score: XXXXX".length(), vbom);
+		scene.attachChild(score);
+	}
+	
 	@Override
 	protected boolean checkProcessing() {
-		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	@Override
-	protected void processEntities(ImmutableBag<Entity> arg0) {
-		// TODO Auto-generated method stub
-		
+	protected void process(Entity entity) {
+		Time time = timeMapper.get(entity);
+		time.updateTime(world.getDelta());
+		score.setText("Score :"+Math.round(time.getTime()));
 	}
 
 }
