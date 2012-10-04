3c33bea72e6c941e0a45e008083273eaf9ea5385
Jakob Svensson <jakob-_-92@hotmail.com>
2012-10-04 13:27:39 +0200

    Added a test for Time

---
+++ test/se/chalmers/avoidance/core/components/TimeTest.java
@@ -0,0 +1,33 @@
+package se.chalmers.avoidance.core.components;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TimeTest {
+	
+	private Time time;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		time = new Time();
+	}
+
+	@Test
+	public void testGetTime() {
+		assertTrue(time.getTime()==0);
+	}
+	
+	@Test
+	public void testUpdateTime(){
+		time.updateTime(3);
+		assertTrue(time.getTime()==3);
+	}
+}
