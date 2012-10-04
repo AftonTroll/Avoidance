3ed5bafaa3b7fdde7076fc6ae1b844ce41670753
Heqir <markus_xtrom@hotmail.com>
2012-10-04 13:23:37 +0200

    Added Buff component.

---
+++ src/se/chalmers/avoidance/core/components/Buff.java
@@ -0,0 +1,57 @@
+/* 
+ * Copyright (c) 2012 Markus Ekström
+ * 
+ * This file is part of Avoidance.
+ * 
+ * Avoidance is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Avoidance is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Avoidance.  If not, see <http://www.gnu.org/licenses/>. 
+ *  
+ */
+package se.chalmers.avoidance.core.components;
+
+/**
+ * Component containing information about buffs.
+ * 
+ * @author Markus Ekström
+ */
+public class Buff {
+	public static enum BuffType{Speed}
+	private int strength;
+	private BuffType type;
+	
+	/**
+	 * Constructs the Buff, giving it the type and strength specified.
+	 * @param type
+	 * @param amount
+	 */
+	public Buff(BuffType type, int strength) {
+		this.type = type;
+		this.strength = strength;
+	}
+	
+	/**
+	 * Returns the buff's type.
+	 * @return The type of the buff.
+	 */
+	public BuffType getType() {
+		return type;
+	}
+	
+	/**
+	 * Returns the buff's strength.
+	 * @return The strength of the buff.
+	 */
+	public int getStrength() {
+		return strength;
+	}
+}
