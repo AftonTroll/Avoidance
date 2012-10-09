5e4a0213c2ba9fb553384b92f85e7affe1f35e7e
AftonTroll <filbry_92@hotmail.com>
2012-10-09 18:14:03 +0200

    Added a Sound component
    The sound component is containing info about what kind of sound it is and
    if it's playing

---
+++ test/se/chalmers/avoidance/core/components/SoundTest.java
@@ -0,0 +1,37 @@
+package se.chalmers.avoidance.core.components;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SoundTest {
+	private static Sound sound;
+	
+	@Before
+	public void setUp() throws Exception {
+		sound = new Sound("bounce");
+	}
+
+	@Test
+	public void testConstructor(){
+		assertTrue(sound.getName().equals("bounce"));
+		assertFalse(sound.isPlaying());
+	}
+	
+	@Test
+	public void testName() {
+		sound.setName("bang");
+		assertTrue(sound.getName().equals("bang"));
+	}
+
+	@Test
+	public void testPlaying() {
+		sound.setPlaying(true);
+		assertTrue(sound.isPlaying());
+		sound.setPlaying(false);
+		assertFalse(sound.isPlaying());
+	}
+
+}
