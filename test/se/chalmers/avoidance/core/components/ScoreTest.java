114471900d9ef5cddc764c31b38a30ab5c3374fe
Jakob Svensson <jakob-_-92@hotmail.com>
2012-10-09 10:38:54 +0200

    Added test for score.java

---
+++ test/se/chalmers/avoidance/core/components/ScoreTest.java
@@ -0,0 +1,42 @@
+package se.chalmers.avoidance.core.components;
+
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ScoreTest {
+	
+	Score score;
+	
+	@Before
+	public void setUp() {
+		score = new Score();
+	}
+	
+	@Test
+	public void testGetScore(){
+		assertTrue(score.getScore()==0);
+	}
+	
+	@Test
+	public void testAddKillScore(){
+		score.addKillScore(100);
+		assertTrue(score.getScore()==100);
+	}
+	
+	@Test
+	public void testAddPowerupScore(){
+		score.addPowerupScore(200);
+		assertTrue(score.getScore()==200);
+	}
+	
+	@Test
+	public void testAddBothScore(){
+		score.addKillScore(100);
+		score.addPowerupScore(200);
+		assertTrue(score.getScore()==300);
+	}
+
+}
