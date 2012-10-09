1d98b469da42ddde719cdfec17ba43b4f8ea33ec
Heqir <markus_xtrom@hotmail.com>
2012-10-09 16:59:51 +0200

    Improved CollisionSystem
    Added pairs and handlers.

--- src/se/chalmers/avoidance/core/systems/CollisionSystem.java
+++ src/se/chalmers/avoidance/core/systems/CollisionSystem.java
@@ -1,5 +1,5 @@
 /* 
- * Copyright (c) 2012 Jakob Svensson
+ * Copyright (c) 2012 Jakob Svensson & Markus Ekström
  * 
  * This file is part of Avoidance.
  * 
@@ -26,28 +26,33 @@ import org.andengine.opengl.vbo.IVertexBufferObject;
 import se.chalmers.avoidance.core.components.Size;
 import se.chalmers.avoidance.core.components.Transform;
 import se.chalmers.avoidance.core.components.Velocity;
-import se.chalmers.avoidance.util.Utils;
 
 import com.artemis.Aspect;
 import com.artemis.ComponentMapper;
 import com.artemis.Entity;
 import com.artemis.EntitySystem;
+import com.artemis.annotations.Mapper;
 import com.artemis.managers.GroupManager;
-import com.artemis.managers.TagManager;
+import com.artemis.utils.Bag;
 import com.artemis.utils.ImmutableBag;
 /**
  * System for handling collision between entities
  * 
  * @author Jakob Svensson
+ * @author Markus Ekström
  *
  */
 public class CollisionSystem extends EntitySystem{
-	
-    private ComponentMapper<Velocity> velocityMapper;
-    private ComponentMapper<Transform> transformMapper;
-    private ComponentMapper<Size> sizeMapper;
-    private CollisionObject collisionObject1;
-    private CollisionObject collisionObject2;
+	@Mapper
+	ComponentMapper<Velocity> velocityMapper;
+	@Mapper
+	ComponentMapper<Transform> transformMapper;
+	@Mapper
+	ComponentMapper<Size> sizeMapper;
+    private Bag<CollisionPair> collisionPairs;
+    private CollisionObject collisionObject1 = new CollisionObject(0, 0, 0, 0);
+    private CollisionObject collisionObject2 = new CollisionObject(0, 0, 0, 0);
+
 	
     /**
      * Constructs a new CollisionSystem 
@@ -56,8 +61,6 @@ public class CollisionSystem extends EntitySystem{
      */
 	public CollisionSystem() {
 		super(Aspect.getAspectForAll(Transform.class, Size.class));
-		collisionObject1 = new CollisionObject(0, 0, 0, 0);
-		collisionObject2 = new CollisionObject(0, 0, 0, 0);
 	}
 	
 	/**
@@ -65,10 +68,11 @@ public class CollisionSystem extends EntitySystem{
 	 */
 	@Override
 	protected void initialize(){
-		velocityMapper = world.getMapper(Velocity.class);
-		transformMapper = world.getMapper(Transform.class);
-		sizeMapper = world.getMapper(Size.class);
+        collisionPairs = new Bag<CollisionPair>();
+        collisionPairs.add(new CollisionPair("PLAYER", "WALLS", new WallCollisionHandler())); 
 	}
+	
+	
 	
 	/**
 	 * Determines if the system should be processed or not
@@ -88,84 +92,30 @@ public class CollisionSystem extends EntitySystem{
 	@Override
 	protected void processEntities(ImmutableBag<Entity> entities) {
 		
-		ImmutableBag<Entity> walls = world.getManager(GroupManager.class).getEntities("WALLS");
-		ImmutableBag<Entity> enemies = world.getManager(GroupManager.class).getEntities("ENEMIES");
-		Entity player = world.getManager(TagManager.class).getEntity("PLAYER");
-		for (int i=0;i<walls.size();i++){
-			if(collisionExists(player, walls.get(i))){
-				handleWallCollision(player, walls.get(i));
-			}
-			for (int j=0;j<enemies.size();j++){
-				if(collisionExists(enemies.get(j), walls.get(i))){
-					handleWallCollision(enemies.get(j), walls.get(i));
-				}
-			}
-		}
-		
-		for (int j=0;j<enemies.size();j++){
-			if(collisionExists(player, enemies.get(j))){
-				//handeEnemyCollision();
-			}
-		}
-		
+        for(int i = 0; collisionPairs.size() > i; i++) {
+            collisionPairs.get(i).checkForCollisions();
+        }
+//		ImmutableBag<Entity> walls = world.getManager(GroupManager.class).getEntities("WALLS");
+//		ImmutableBag<Entity> enemies = world.getManager(GroupManager.class).getEntities("ENEMIES");
+//		Entity player = world.getManager(TagManager.class).getEntity("PLAYER");
+//		for (int i=0;i<walls.size();i++){
+//			if(collisionExists(player, walls.get(i))){
+//				handleWallCollision(player, walls.get(i));
+//			}
+//			for (int j=0;j<enemies.size();j++){
+//				if(collisionExists(enemies.get(j), walls.get(i))){
+//					handleWallCollision(enemies.get(j), walls.get(i));
+//				}
+//			}
+//		}
+//		
+//		for (int j=0;j<enemies.size();j++){
+//			if(collisionExists(player, enemies.get(j))){
+//				//handeEnemyCollision();
+//			}
+//		}	
 	}
-	
-	
-	private void handleWallCollision(Entity player, Entity wall){		
-		Size wallSize = sizeMapper.get(wall);
-		Transform wallTransform = transformMapper.get(wall);
-		Velocity playerVelocity = velocityMapper.get(player);
-		Size playerSize = sizeMapper.get(player);
-		Transform playerTransform = transformMapper.get(player);
 		
-		float wallWidth = wallSize.getWidth();
-		float wallHeight = wallSize.getHeight();
-		float wallX = wallTransform.getX(); 
-		float wallY = wallTransform.getY();		
-		float angle = playerVelocity.getAngle();
-		float newAngle = angle;
-		
-		//Check if player collides with horizontal side or vertical side
-		if(playerTransform.getX()+playerSize.getWidth()/2>wallX&&playerTransform.getX()+playerSize.getWidth()/2<wallX+wallWidth){
-			newAngle = flipVertical(angle);
-			if(angle>Math.PI){
-				//Collision on lower side of the wall
-				playerTransform.setY(wallY+wallHeight);
-			}else{
-				//Collision on upper side of the wall
-				playerTransform.setY(wallY-playerSize.getHeight());
-			}
-			
-		}else if(playerTransform.getY()+playerSize.getHeight()/2>wallY&&playerTransform.getY()+playerSize.getHeight()/2<wallY+wallHeight){
-			newAngle = flipHorizontal(angle);
-			if(angle>Math.PI/2&&angle<(Math.PI*3)/2){
-				//Collision on right side of wall
-				playerTransform.setX(wallX+wallWidth);
-			}else{
-				//Collision on left side of wall
-				playerTransform.setX(wallX-playerSize.getWidth());
-			}
-			
-		}else{
-			//Corner or almost corner collision			
-			newAngle = Utils.reverseAngle(angle);
-		}
-		playerVelocity.setAngle(newAngle);
-	}
-	
-	private float flipVertical(float angle){ 
-		  float newAngle=angle*-1; 
-		  return newAngle;
-	}
-	
-	private float flipHorizontal(float angle){
-		  //Translate and then flip vertical
-		float newAngle = angle + (float) Math.PI/2;
-		  newAngle = flipVertical(newAngle);
-		  newAngle -= Math.PI/2;
-		  return newAngle;
-	}
-	
 	/**
 	 * Checks if two entities is colliding with each other 
 	 * 
@@ -201,8 +151,10 @@ public class CollisionSystem extends EntitySystem{
 		collisionObject2.setHeight(e2Height);
 		
 		return collisionObject1.collidesWith(collisionObject2);
-		
 	}
+	
+	
+
 	
 	/**
 	 * An object used to check for collision with Andengine's collision detection
@@ -233,5 +185,28 @@ public class CollisionSystem extends EntitySystem{
 		}
 		
 	}
+	
+    private class CollisionPair {
+        private ImmutableBag<Entity> groupEntitiesA;
+        private ImmutableBag<Entity> groupEntitiesB;
+        private CollisionHandler handler;
 
+        public CollisionPair(String group1, String group2, CollisionHandler handler) {
+                groupEntitiesA = world.getManager(GroupManager.class).getEntities(group1);
+                groupEntitiesB = world.getManager(GroupManager.class).getEntities(group2);
+                this.handler = handler;
+        }
+
+        public void checkForCollisions() {
+                for(int a = 0; groupEntitiesA.size() > a; a++) {
+                        for(int b = 0; groupEntitiesB.size() > b; b++) {
+                                Entity entityA = groupEntitiesA.get(a);
+                                Entity entityB = groupEntitiesB.get(b);
+                                if(collisionExists(entityA, entityB)) {
+                                        handler.handleCollision(entityA, entityB);
+                                }
+                        }
+                }
+        }
+    }
 }
