34fdf857b6e5772ceff1643f3e94cc0ae9d2070e
Jakob Svensson <jakob-_-92@hotmail.com>
2012-10-14 12:11:02 +0200

    Added javadoc to enemycollisionhandler

--- src/se/chalmers/avoidance/core/collisionhandlers/EnemyCollisionHandler.java
+++ src/se/chalmers/avoidance/core/collisionhandlers/EnemyCollisionHandler.java
@@ -5,15 +5,30 @@ import se.chalmers.avoidance.core.components.Jump;
 import com.artemis.ComponentMapper;
 import com.artemis.Entity;
 import com.artemis.World;
-
+/**
+ * Handles collision between player and enemies.
+ * 
+ * @author Jakob Svensson
+ *
+ */
 public class EnemyCollisionHandler implements CollisionHandler{
 	
 	private World world;
 	
+	/**
+	 * Constructs a EnemyCollisionHandler.
+	 * 
+	 * @param world The world.
+	 */
 	public EnemyCollisionHandler (World world){
 		this.world=world;
 	}
-	
+	/**
+	 * Takes the player and enemy and handles the collision between them
+	 * 
+	 * @param player The player.
+	 * @param enemy The enemy.
+	 */
 	public void handleCollision(Entity player, Entity enemy) {
 		//Handle collison between enemy and player
 		ComponentMapper<Jump> jumpMapper = world.getMapper(Jump.class);
