4fa3d238612830e970a65aa51759bbf6513ade9c
Jakob Svensson <jakob-_-92@hotmail.com>
2012-10-20 14:47:48 +0200

    Cleaned up score

--- src/se/chalmers/avoidance/core/components/Score.java
+++ src/se/chalmers/avoidance/core/components/Score.java
@@ -22,36 +22,36 @@ package se.chalmers.avoidance.core.components;
 
 import com.artemis.Component;
 /**
- * Component holding the score
+ * Component holding the score.
  * 
  * @author Jakob Svensson
  *
  */
 public class Score extends Component {
-	
+
 	private int killScore = 0;
 	private int powerupScore = 0;
-	
+
 	/**
-	 * Returns the current score from killing enemies and picking up power-ups
+	 * Returns the current score from killing enemies and picking up power-ups.
 	 * 
 	 * @return the score 
 	 */
 	public int getScore(){
 		return killScore+powerupScore;
 	}
-	
+
 	/**
-	 * Adds score from killing enemies
+	 * Adds score from killing enemies.
 	 * 
 	 * @param score the score to be added
 	 */
 	public void addKillScore(int score){
 		killScore+=score;
 	}
-	
+
 	/**
-	 * Adds score from picking up power-ups
+	 * Adds score from picking up power-ups.
 	 * 
 	 * @param score the score to be added
 	 */
