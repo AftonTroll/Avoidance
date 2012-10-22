251621b4aaf377a244b23437acdea0ab5e4c4b9c
Heqir <markus_xtrom@hotmail.com>
2012-10-22 15:17:40 +0200

    Added ImmortalTest class

---
+++ test/se/chalmers/avoidance/core/components/ImmortalTest.java
@@ -0,0 +1,19 @@
+package se.chalmers.avoidance.core.components;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ImmortalTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
