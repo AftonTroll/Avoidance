35000e0f5585cd064ac26d57327541ebc57bb3a2
Jakob Svensson <jakob-_-92@hotmail.com>
2012-10-22 14:51:20 +0200

    Added javadoc to GameConstants

--- src/se/chalmers/avoidance/constants/GameConstants.java
+++ src/se/chalmers/avoidance/constants/GameConstants.java
@@ -1,25 +1,60 @@
 package se.chalmers.avoidance.constants;
-
+/**
+ * Container of constants for mapping groups and tags.
+ * 
+ * @author Jakob Svensson
+ *
+ */
 public final class GameConstants {
 	
+	/**
+	 * Identifier for the player tag.
+	 */
 	public static final String PLAYER_TAG = "PLAYER";
 	
+	/**
+	 * Identifier for the score tag.
+	 */
 	public static final String SCORE_TAG = "SCORE";
 	
+	/**
+	 * Identifier for the player group.
+	 */
 	public static final String PLAYER_GROUP = "PLAYER";
 	
+	/**
+	 * Identifier for the wall group.
+	 */
 	public static final String WALLS_GROUP = "WALLS";
 	
+	/**
+	 * Identifier for the movingentities group.
+	 */
 	public static final String MOVINGENTITIES_GROUP = "MOVINGENTITIES";
 	
+	/**
+	 * Identifier for the powerups group.
+	 */
 	public static final String POWERUPS_GROUP = "POWERUPS";
 	
+	/**
+	 * Identifier for the pitobstacle group.
+	 */
 	public static final String PITOBSTACLES_GROUP = "PITOBSTACLES";
 	
+	/**
+	 * Identifier for the killplayerobstacle group.
+	 */
 	public static final String KILLPLAYEROBSTACLES_GROUP = "KILLPLAYEROBSTACLES";
 	
+	/**
+	 * Identifier for the enemies group.
+	 */
 	public static final String ENEMIES_GROUP = "ENEMIES"; 
 	
+	/**
+	 * Identifier for the circleshapes group.
+	 */
 	public static final String CIRCLESHAPES_GROUP = "CIRCLESHAPES";
 	
 }
