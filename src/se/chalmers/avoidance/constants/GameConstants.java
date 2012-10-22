3b3cfb92096dd60217d4515870085d2e9d118096
Jakob Svensson <jakob-_-92@hotmail.com>
2012-10-22 10:54:19 +0200

    Added a constant class for game constants

---
+++ src/se/chalmers/avoidance/constants/GameConstants.java
@@ -0,0 +1,23 @@
+package se.chalmers.avoidance.constants;
+
+public final class GameConstants {
+	
+	public static final String PLAYER_TAG = "PLAYER";
+	
+	public static final String PLAYER_GROUP = "PLAYER";
+	
+	public static final String WALLS_GROUP = "WALLS";
+	
+	public static final String MOVINGENTITIES_GROUP = "MOVINGENTITIES";
+	
+	public static final String POWERUPS_GROUP = "POWERUPS";
+	
+	public static final String PITOBSTACLES_GROUP = "PITOBSTACLES";
+	
+	public static final String KILLPLAYEROBSTACLES_GROUP = "KILLPLAYEROBSTACLES";
+	
+	public static final String ENEMIES_GROUP = "ENEMIES"; 
+	
+	public static final String CIRCLESHAPES_GROUP = "CIRCLESHAPES";
+	
+}
